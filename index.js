2556707aeceadcdbf477a36f2cdffd488a2ae737
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2023-10-30 20:46:49 +0100

    saving files

--- index.js
+++ index.js
@@ -29,14 +29,25 @@ function adjustNote(note) {
 	}
 }
 
-function createNewSimulation(note, taux) {
+async function createNewSimulation(note, taux, creation) {
 	const previousDiv = document.getElementById("previous");
 	const previousText = document.getElementById("previous-text");
-	
+
+	const newContent =
+		"<article>" +
+		`<div class="type"><img src="./car.png"><p>${getVehicleType()}</p><h3>${await data.then((response) => {return response.type_vehicule[getVehicleType()].note})}/10</h3></div>` +
+		`<div class="energy"><img src="./energy.png"><p>${getVehicleEnergy()}</p><h3>${await data.then((response) => {return response.energie[getVehicleEnergy()]})}/10</h3></div>` +
+		`<div class="travel"><img src="./wheel.png"><p>${getVehicleTravel()}K</p><h3>${await data.then((response) => {return response.kilometrage[getVehicleTravel()]})}/10</h3></div>` +
+		`<div class="creation"><img src="./calendar.png"><p>${getVehicleCreation()}</p><h3>${await data.then((response) => {return response.annee[creation]})}/10</h3></div>` +
+		`<div class="passengers"><img src="./passengers.png"><p>${getVehiclePassengers()}</p><h3>${await data.then((response) => {return response.passagers[getVehiclePassengers()]})}%</h3></div>` +
+		`<div class="previous_result"><p>Note : <span class="green">${note}/40</span></p><p>Taux d\emprunt : <span class="green">${taux}%</span></p></div>` +
+		"</article>";
+
 	if (previousText.innerText == "Vois n'avez aucune simulations !") {
 		previousText.innerText = ""
+		previousDiv.innerHTML += newContent;
 	} else {
-		
+		previousDiv.innerHTML += newContent
 	}
 }
 
@@ -49,6 +60,7 @@ async function result() {
 	const error = document.getElementById("error");
 	let note = 0;
 	let taux = 0;
+	let creation = null
 
 	if (
 		getVehicleCreation() == "" ||
@@ -80,9 +92,10 @@ async function result() {
 				parseInt(dates_btw[0]) <= date &&
 				date < parseInt(dates_btw[1])
 			) {
+				creation = elem
 				note += parseInt(
 					await data.then((response) => {
-						return parseInt(response.annee[elem]);
+						return parseInt(response.annee[creation]);
 					})
 				);
 			}
@@ -93,10 +106,9 @@ async function result() {
 		const notes = await data.then((response) => {
 			return response.taux;
 		});
-		console.log(notes);
+		
 		for (elem in notes) {
 			const notes_btw = elem.split("-");
-			console.log(notes_btw);
 			if (
 				parseInt(notes_btw[0]) <= note &&
 				note < parseInt(notes_btw[1])
@@ -115,5 +127,6 @@ async function result() {
 		}
 
 		createResult(note, taux);
+		createNewSimulation(note, taux, creation);
 	}
 }
