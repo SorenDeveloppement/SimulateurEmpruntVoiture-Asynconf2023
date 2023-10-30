8d1687efcd6df44389762a87bcdd94140bbf1877
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2023-10-30 21:48:58 +0100

    saving files

--- index.js
+++ index.js
@@ -38,19 +38,39 @@ async function createNewSimulation(note, taux, creation) {
 
 	const newContent =
 		"<article>" +
-		`<div class="type"><img src="./car.png"><p>${getVehicleType()}</p><h3>${await data.then((response) => {return response.type_vehicule[getVehicleType()].note})}/10</h3></div>` +
-		`<div class="energy"><img src="./energy.png"><p>${getVehicleEnergy()}</p><h3>${await data.then((response) => {return response.energie[getVehicleEnergy()]})}/10</h3></div>` +
-		`<div class="travel"><img src="./wheel.png"><p>${getVehicleTravel()}K</p><h3>${await data.then((response) => {return response.kilometrage[getVehicleTravel()]})}/10</h3></div>` +
-		`<div class="creation"><img src="./calendar.png"><p>${getVehicleCreation()}</p><h3>${await data.then((response) => {return response.annee[creation]})}/10</h3></div>` +
-		`<div class="passengers"><img src="./passengers.png"><p>${getVehiclePassengers()}</p><h3>${await data.then((response) => {return response.passagers[getVehiclePassengers()]})}%</h3></div>` +
+		`<div class="type"><img src="./car.png"><p>${getVehicleType()}</p><h3>${await data.then(
+			(response) => {
+				return response.type_vehicule[getVehicleType()].note;
+			}
+		)}/10</h3></div>` +
+		`<div class="energy"><img src="./energy.png"><p>${getVehicleEnergy()}</p><h3>${await data.then(
+			(response) => {
+				return response.energie[getVehicleEnergy()];
+			}
+		)}/10</h3></div>` +
+		`<div class="travel"><img src="./wheel.png"><p>${getVehicleTravel()}K</p><h3>${await data.then(
+			(response) => {
+				return response.kilometrage[getVehicleTravel()];
+			}
+		)}/10</h3></div>` +
+		`<div class="creation"><img src="./calendar.png"><p>${getVehicleCreation()}</p><h3>${await data.then(
+			(response) => {
+				return response.annee[creation];
+			}
+		)}/10</h3></div>` +
+		`<div class="passengers"><img src="./passengers.png"><p>${getVehiclePassengers()}</p><h3>${await data.then(
+			(response) => {
+				return response.passagers[getVehiclePassengers()];
+			}
+		)}%</h3></div>` +
 		`<div class="previous_result"><p>Note : <span class="green">${note}/40</span></p><p>Taux d\emprunt : <span class="green">${taux}%</span></p></div>` +
 		"</article>";
 
 	if (previousText.innerText == "Vous n'avez aucune simulation !") {
-		previousText.innerText = ""
+		previousText.innerText = "";
 		previousDiv.innerHTML += newContent;
 	} else {
-		previousDiv.innerHTML += newContent
+		previousDiv.innerHTML += newContent;
 	}
 }
 
@@ -65,7 +85,7 @@ async function result() {
 	const error = document.getElementById("error");
 	let note = 0;
 	let taux = 0;
-	let creation = null
+	let creation = null;
 
 	// Check if an input is empty
 	if (
@@ -93,13 +113,10 @@ async function result() {
 		});
 
 		for (elem in dates) {
-			const dates_btw = elem.split("-");
+			const datesBtw = elem.split("-");
 			const date = parseInt(getVehicleCreation());
-			if (
-				parseInt(dates_btw[0]) <= date &&
-				date < parseInt(dates_btw[1])
-			) {
-				creation = elem
+			if (parseInt(datesBtw[0]) <= date && date < parseInt(datesBtw[1])) {
+				creation = elem;
 				note += parseInt(
 					await data.then((response) => {
 						return parseInt(response.annee[creation]);
@@ -113,24 +130,26 @@ async function result() {
 		const notes = await data.then((response) => {
 			return response.taux;
 		});
-		
+
 		for (elem in notes) {
-			const notes_btw = elem.split("-");
+			const notesBtw = elem.split("-");
 			if (
-				parseInt(notes_btw[0]) <= note &&
-				note <= parseInt(notes_btw[1])
+				parseInt(notesBtw[0]) <= note &&
+				note <= parseInt(notesBtw[1])
 			) {
-				console.log(true)
-				taux += parseFloat(Number(
-					await data.then((response) => {
-						return (
-							parseFloat(response.taux[elem]) +
-							parseFloat(
-								response.passagers[getVehiclePassengers()]
-							)
-						);
-					})
-				).toFixed(2));
+				console.log(true);
+				taux += parseFloat(
+					Number(
+						await data.then((response) => {
+							return (
+								parseFloat(response.taux[elem]) +
+								parseFloat(
+									response.passagers[getVehiclePassengers()]
+								)
+							);
+						})
+					).toFixed(2)
+				);
 			}
 		}
 
